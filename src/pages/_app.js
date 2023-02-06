9af76e0ed6f5d8512413f5d7dcf1ed147c0378d6
Santi Gimenez <syberiaarg@gmail.com>
2023-02-06 15:04:08 -0300

    Upload - First Package

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
+import Image from 'next/image'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => (
+
+  <>
+    <Head>
+      <title>Pokenext App</title>
+      <meta name="description" content="Pokemonsters App - Nextjs Practice" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Component {...pageProps} />
+  </>
+)
+
+export default App
