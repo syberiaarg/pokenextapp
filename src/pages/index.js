a12c729181b4e97f2e6989a5bb72175e31598bb6
Santiago Gimenez <sgimenez1995@hotmail.com>
2023-03-07 20:52:01 -0300

    Structuring Base Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import pokeHeader from "../assets/pokenextlogo.png"
+
+
 export default function Home() {
   return (
-    <>
-      <h1>Time to code...</h1>
-    </>
+    <div>
+      <header>
+        <Image src={pokeHeader} />
+      </header>
+      <aside>
+        <ul>
+          <li>Home</li>
+          <li>Team</li>
+          <li>Trainer</li>
+          <li>Get Help</li>
+        </ul>
+      </aside>
+      <footer>
+        <div>PokenextApp © 2023</div>
+      </footer>
+    </div>
   );
 }
