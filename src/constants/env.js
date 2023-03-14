15ab22015aa4cdc6ae7557b106953210b10b78b9
Santiago Gimenez <sgimenez1995@hotmail.com>
2023-03-14 16:16:11 -0300

    Fix error with enviroment variables and the process of env file

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_BASE_URL;
-export const API_POKEMON_URL = process.env.REACT_APP_API_POKEMON_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const API_POKEMON_URL = process.env.NEXT_PUBLIC_API_POKEMON_URL;
